30662964932fae124356e9a90bf20f8b75b2b4ad
iamsyahirah <syahirahmohammad89@gmail.com>
2024-10-11 16:01:41 +0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script async src="https://cdn.jsdelivr.net/npm/preline/dist/preline.js"></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
       </body>
     </html>
   );
