f4e60b5ac851bf4a3e083cdff8912bdc35cf2364
iamsyahirah <syahirahmohammad89@gmail.com>
2024-10-13 23:09:06 +0800

    feat: textarea section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Image from "next/image";
+import React, { useState, ChangeEvent } from 'react';
+import TextArea from '../components/Inputs/TextArea';
 
 export default function Home() {
+
+  const [sourceText, setSourceText] = useState<string>("");
+
   return (
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
@@ -10,6 +17,25 @@ export default function Home() {
           <div className="text-center">
             <h1 className="text-4xl sm:text-6xl font-bold text-slate-900">Velo<span className="text-[#f87315]">Translate</span></h1>
             <p className="mt-3 text-slate-700">VeloTranslate: Bridging Voices, Connecting Worlds</p>
+
+            <div className="mt-7 st:mt-12 mx-auto max-w-3xl relative">
+              <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
+                <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-white p-4 dark:bg-neutral-900 dark:border-gray-500 shadow-neutral-300">
+                  <TextArea
+                    id="source-language"
+                    value={sourceText}
+                    onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+                      setSourceText(e.target.value);
+                    }}
+                    placeholder="Source Language"
+                  />
+                  <div className="flex flex-row justify-between w-full">
+                    <span className="cursor-pointer flex space-x-2 flex-row"></span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
